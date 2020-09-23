3a3be3007c22b45ffb4352481f82137f5ea3bbf7
Merilyn Mastrangelo <64098877+MerilynMastrangelo@users.noreply.github.com>
2020-09-23 18:43:21 +0100

    first commit - responsive website

---
+++ navibar.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('ul');
+    const navLinks = document.querySelectorAll('ul li');
+    const body = document.querySelector('body');
+    //Toggle Nav
+    burger.addEventListener('click', () => {
+            nav.classList.toggle('nav-active');
+        //Animate Links
+        navLinks.forEach((link, index)=>{
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        //Animate burger
+        burger.classList.toggle('toggle');
+        //body darker
+        body.classList.toggle('body-background');
+    });
+}
+navSlide();
